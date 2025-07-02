101c703503d038c035489c5e4aba8c83b71b5008
Faiyaz <faiyaz.samin@gmail.com>
2025-07-02 16:42:02 +0600

    update stripe version

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -13,7 +13,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-04-30.basil'
+  apiVersion: '2025-06-30.basil'
 });
 
 export async function createCheckoutSession({
